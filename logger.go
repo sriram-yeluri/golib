ce6a97cfea09d98ced680d997fc68fd5e0d5e41d
Sriram Yeluri <yeluris@gmail.com>
2023-03-18 12:25:37 +0100

    Updated logger outputs

--- logger.go
+++ logger.go
@@ -22,7 +22,7 @@ func init() {
 	// Warning = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
 	// Error = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 
-	Info = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-	Warning = log.New(os.Stdout, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
-	Error = log.New(os.Stdout, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Info = log.New(os.Stdout, "INFO :: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Warning = log.New(os.Stdout, "WARNING :: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Error = log.New(os.Stdout, "ERROR :: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
