afbd449cf050d19aa108c247822fd5d0f91a1bb1
Sriram Yeluri <yeluris@gmail.com>
2023-03-22 21:14:52 +0100

    Added testing for display

---
+++ display_test.go
@@ -0,0 +1,16 @@
+package golib
+
+import (
+	"testing"
+)
+
+func TestPrintHttpResponseBody(t *testing.T) {
+	var c = new(Client)
+	c.SetBaseURL("http://ip.jsontest.com/")
+	c.SetBasicAuth("username","password")
+	resp, err := c.SendHttpRequest("GET", "")
+	if err != nil {
+		Error.Print(err)
+	}
+	PrintHttpResponseBody(resp)
+}
