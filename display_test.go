6e02933c1ffb604932598b9086da0c7f72baf224
Sriram Yeluri <yeluris@gmail.com>
2023-03-22 21:47:23 +0100

    added test function

--- display_test.go
+++ display_test.go
@@ -7,7 +7,7 @@ import (
 func TestPrintHttpResponseBody(t *testing.T) {
 	var c = new(Client)
 	c.SetBaseURL("http://ip.jsontest.com/")
-	c.SetBasicAuth("username","password")
+	c.SetBasicAuth("username", "password")
 	resp, err := c.SendHttpRequest("GET", "")
 	if err != nil {
 		Error.Print(err)
